7ccc01307790ad1f119829320f9628c4e07b4a7b
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2024-09-25 15:46:37 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,23 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const stack = [];
-    const map = {
-        '(':')',
-        '{':'}',
-        '[':']'
-    };
-    for(let i = 0; i < s.length; i++){
-        if(s[i] === '(' || s[i] === '{' || s[i] === '['){
-            stack.push(s[i]);
-        }else{
-            const last = stack.pop();
-            if(map[last] !== s[i]){
-                return false;
-            }
-        }
-    }
-    return stack.length === 0;
+    // const stack = [];
+    // const map = {
+    //     '(':')',
+    //     '{':'}',
+    //     '[':']'
+    // };
+    // for(let i = 0; i < s.length; i++){
+    //     if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+    //         stack.push(s[i]);
+    //     }else{
+    //         const last = stack.pop();
+    //         if(map[last] !== s[i]){
+    //             return false;
+    //         }
+    //     }
+    // }
+    // return stack.length === 0;
+
+    if(s.length % 2 !== 0)
 };
 
 module.exports = { isValid };
